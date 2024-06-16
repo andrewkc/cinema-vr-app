93dc457eb589a06a3232db06938f32c59f982062
MauBC <mauro.bobadilla@utec.edu.pe>
2024-06-16 16:18:07 -0500

    Added multiplayer movement

---
+++ cinemavr/Assets/Scripts/NetworkInputData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using UnityEngine;
+
+public struct NetworkInputData : INetworkInput
+{
+    public Vector3 direction;
+}
