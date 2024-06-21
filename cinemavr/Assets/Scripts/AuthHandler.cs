e96a0fc0899059c4916da00926dbe322f8ca6520
andrewkc <andresjkcc@gmail.com>
2024-06-21 06:11:21 -0500

    add2

--- cinemavr/Assets/Scripts/AuthHandler.cs
+++ cinemavr/Assets/Scripts/AuthHandler.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class AuthHandler : MonoBehaviour
 {
-    const string LAST_EMAIL_KEY = "LAST_EMAIL", LAST_PASSWORD = "LAST_PASSWORD";
+    const string TITLE_ID = "94004";
 
     #region Register
     [Header("Register UI:")]
@@ -15,18 +17,67 @@ public class AuthHandler : MonoBehaviour
 
     public void OnRegisterPressed() 
     {
-        Register(registerEmail.text, registerUsername.text, registerPassword.text);
+        Debug.Log(registerEmail.text + " " + registerUsername.text + " " + registerPassword.text);
+        return;
+        //Register(registerEmail.text, registerUsername.text, registerPassword.text);
     }
 
     private void Register(string email, string username, string password)
-
-
+    {
+        password = Encrypt(password);
+        var registerRequest = new RegisterPlayFabUserRequest {
+            Email = email,
+            Username = username,
+            DisplayName = username,
+            Password = password
+        };
+        PlayFabClientAPI.RegisterPlayFabUser(registerRequest, OnRegisterRequestSuccess(username, password) , PlayFabFailure);
+    }
+    private void OnRegisterRequestSuccess() {
+        Debug.Log("User Registered");
+        Login(username, password);
+    }
     #endregion
 
     #region Login
     [Header("Login UI:")]
     // [SerializedField] TMP_ImputField registerEmail;
-    [SerializedField] TMP_ImputField registerUsername;
-    [SerializedField] TMP_ImputField registerPassword;
+    [SerializedField] TMP_ImputField loginUsername;
+    [SerializedField] TMP_ImputField loginPassword;
+
+    public void OnLoginPressed()
+    {
+        Login(loginUsername.text, loginPassword.text);
+    }
+
+    private void Login(string email, string password)
+    {
+        password = Encrypt(password);
+        var loginRequest = new LoginWithPlayFabRequest {
+            TitleId = TITLE_ID,
+            Password = password,
+            Username = username
+        };
+        PlayFabClientAPI.LoginWithPlayFab(loginRequest, OnLoginRequestSuccess, PlayFabFailure);
+    }
+    private void OnLoginRequestSuccess() {
+        Debug.Log("User Logged");
+    }
     #endregion
+    
+    private string Encrypt(string pw) 
+    {
+        System.Security.Crytography.MD5CryptpServiceProvider x = new System.Security.Crytography.MD5CryptpServiceProvider();
+        byte[] epw = System.Text.Encoding.UTF8.GetBytes(pw);
+        epw = x.ComputedHash(epw);
+        System.Text.StringBuilder s = new System.Text.StringBuilder();
+        foreach (byte b in epw) {
+            s.Append(b.ToString("x2").ToLower());
+        }
+        return s.ToString();
+    }
+    private void PlayFabFailure(PlayFabError error)
+    {
+        Debug.Log(error.Error + " : " + error.GenerateErrorReport());
+    }
 }
